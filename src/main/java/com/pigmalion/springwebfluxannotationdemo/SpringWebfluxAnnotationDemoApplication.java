21618cd2c4b1056eb2cb9897cea3faecfa1d0318
Lucia Rumbo <lurumbo18@gmail.com>
2020-07-01 11:22:26 -0300

    Initial commit

---
+++ src/main/java/com/pigmalion/springwebfluxannotationdemo/SpringWebfluxAnnotationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pigmalion.springwebfluxannotationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxAnnotationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxAnnotationDemoApplication.class, args);
+	}
+
+}
